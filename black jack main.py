1b255c22d7dd858e069a45049bf3be55fe04f640
George Munday <102688916+GeorgeMunday@users.noreply.github.com>
2024-12-03 12:38:02 +0000

    update

--- black jack main.py
+++ black jack main.py
@@ -1,33 +1,40 @@
 import random
-def main(bank):
-    bet = int(input("how much would you like to bet"))
-    bank = bank - bet
-    print(bank)
-    deck = [1,2,3,4,5,6,7,8,9,10,10,10,10,11] * 4
-    print(deck)
+def game(bank):
+    bet =(input("how much would you like to bet"))
     
-    
-
-
-
-print("Welcome to Balck Jack")  #introduction
-play =  input("are you over the age of 18 (yes/no)").lower() #.lower so the input is lower case so doesnt mess up if statment
-
-if play == "yes":
-    play = True#activating the while loop
-else:
-    play = False
-    print("you are not above the legal age to play this game")
-    
-while play == True:
-    bank = input("how much money do you want to put in your bank")
-    bank_int = bank.isnumeric()#validation that it is a number so the code doesnt break
-    
-    if bank_int == True:
-        print(f"you put in £{bank}")
-        break
+    if bet.isnumeric(): 
+        bet = int(bet)
+        while True:
+            bank = bank - bet#how much you have left in your bank
+            print(f" you have {bank} in your bank")
+            deck = [1,2,3,4,5,6,7,8,9,10,10,10,10,11] * 4
+            break
     else:
-        print("please enter a valid input")
-print("here are the rules;\n1.your goal is to get the closes you can to 21\n2.the dealer will be doing the same if they beat you you lose")
-print("if you win")
-main(bank)
+        print("invalid input")
+        
+
+def main():
+    print("Welcome to Balck Jack")  #introduction
+    play =  input("are you over the age of 18 (yes/no)").lower() #.lower so the input is lower case so doesnt mess up if statment
+
+    if play == "yes":
+        play = True#activating the while loop
+    elif play == "no":
+        print("you are not the lega; age to play this game")
+    else:
+        play = False
+        print("invalid input")
+        
+    while play == True:
+        bank = input("how much money do you want to put in your bank")
+        bank_int = bank.isnumeric()#validation that it is a number so the code doesnt break
+        
+        if bank_int == True:
+            print(f"you put in £{bank}")
+            break
+        else:
+            print("please enter a valid input")
+
+        game(bank)
+
+main()
