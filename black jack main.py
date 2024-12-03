ee9f9aff1402169524a901ae8ac4e996688f2fb8
George Munday <102688916+GeorgeMunday@users.noreply.github.com>
2024-12-03 09:38:50 +0000

    update

--- black jack main.py
+++ black jack main.py
@@ -3,11 +3,8 @@ def main(bank):
     bet = int(input("how much would you like to bet"))
     bank = bank - bet
     print(bank)
-    your_card1 = random.randint(1,11)
-    your_card2 = random.randint(1,11)
-    dealer_hand = random.randint(1,11)
-    dealer_hand = random.randint(1,11)
-    
+    deck = [1,2,3,4,5,6,7,8,9,10,10,10,10,11] * 4
+    print(deck)
     
     
 
