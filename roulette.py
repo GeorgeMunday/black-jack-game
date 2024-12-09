bca78d58a2ef1e448a8e1392e85a768db0986823
George Munday <102688916+GeorgeMunday@users.noreply.github.com>
2024-12-09 12:28:34 +0000

    update

--- roulette.py
+++ roulette.py
@@ -3,25 +3,25 @@ def result_game():
     list1 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22]
     number = random.choice ( list1)
     return number
+
 def exit_game(bank):
     profit = bank - 100
     print(f"you profited £{profit}")
     exit()
     
 
+
 def roulette (bank):
-    choice1 = input("what would you like to bet on")
-
-
-
-    result = result_game(bank)
+    
+    result = result_game()
+    print(result)
 
 
 def main():
     bank = 0
     play = ""
     
-    # Welcome message
+    
     print("\n Welcome to roulette! ")
     print(" Try your luck and win big! ")
     print("-" * 40)
@@ -39,8 +39,12 @@ def main():
     while play == True :
         bank = 100
         print(f"you have this much in your bank{bank}")
-        
-        
-            
-# Run the game
+        roulette(bank)
+        break
+
+
+
+
+
+       
 main()
