fde49878b89dfc307422a5e28881371d51d81da9
George Munday <102688916+GeorgeMunday@users.noreply.github.com>
2024-12-11 12:51:09 +0000

    update

--- roulette.py
+++ roulette.py
@@ -1,6 +1,7 @@
 import random
+import time
 def result_game():
-    list1 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22]
+    list1 = [random.randint(0,36)]
     number = random.choice ( list1)
     return number
 
@@ -12,10 +13,108 @@ def exit_game(bank):
 
 
 def roulette (bank):
-    
-    result = result_game()
-    print(result)
-
+    print(f"\n You have £{bank} left in your bank.\n")
+    bet =input("How much would you like to bet? £")
+    if bet == "0":
+        exit_game()                      
+    else:
+        if bet.isnumeric(): 
+            bet = int(bet)
+            bank = bank - bet
+            choice1 = ""
+            choice2 = ""
+            result = result_game()
+            condition = ""
+            status = ""
+            choice1 = input("what would you like to bet on:\n 1. colour,2. odd or even ,3. number\n ")
+            while choice1 == "1":
+                print(f"you have {bank} in your bank ")
+                choice2 = input("would you like to bet on red or black")
+                print("spinning...")
+                time.sleep(1)
+                print("spinning...")
+                time.sleep(1)
+                print("spinning...")
+                print(f"it landed on {result}")
+                if result % 2 == 0:
+                    condition = "red"
+                    print(f"it is {condition}")
+                if result % 2 != 0:
+                    condition = "black"
+                    print(f"it is {condition}")
+                if condition == choice2:
+                    status = "win"
+                    print("you win")
+                    
+                else:
+                    status = "lose"
+                    print("you lose")
+                    
+                    
+                
+                if status == "win":
+                    bet = bet * 2
+                    bank += bet
+                else:
+                    bank = bank
+                    
+            while choice1 == "2":
+                print(f"you have {bank} in your bank ")
+                choice2 = input("would you like to bet on odd or even")
+                print("spinning...")
+                time.sleep(1)
+                print("spinning...")
+                time.sleep(1)
+                print("spinning...")
+                print(f"it landed on {result}")
+                if result % 2 == 0:
+                    condition = "even"
+                    print(f"it is {condition}")
+                if result % 2 != 0:
+                    condition = "odd"
+                    print(f"it is {condition}")
+                if condition == choice2:
+                    status = "win"
+                    print("you win")
+                else:
+                    status = "win"
+                    print("you lose")
+                
+                if status == "win":
+                    bet = bet * 2
+                    bank += bet
+                else:
+                    bank = bank
+                    
+            while choice1 == "3":
+                print(f"you have {bank} in your bank ")
+                choice2 = input("what number do you want to bet on")
+                int(choice2)
+                print("spinning...")
+                time.sleep(1)
+                print("spinning...")
+                time.sleep(1)
+                print("spinning...")
+                print(f"it landed on {result}")
+                if condition == choice2:
+                    print("you win")
+                    status = "win"
+                    break
+                else:
+                    status = "lose"
+                    print("you lose")
+                    
+                if status == "win":
+                    bet = bet * 36
+                    bank += bet
+                else:
+                    bank = bank
+                    
+        else:
+            print("invalid input")
+            roulette(bank)
+            
+        
 
 def main():
     bank = 0
